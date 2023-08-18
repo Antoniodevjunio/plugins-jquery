033945dbabde48e3792895f677b82cffb189491a
Antoniodevjunio <128420238+Antoniodevjunio@users.noreply.github.com>
2023-08-18 00:25:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
-    // Aplicar máscaras usando jQuery Mask Plugin
+   
+    $('#telefone').mask('(00) 0000-0000');
     $('#cpf').mask('000.000.000-00');
     $('#cep').mask('00000-000');
 
-    // Submissão do formulário
     $('#formulario').submit(function(event) {
-        event.preventDefault(); // Impedir o envio padrão
+        event.preventDefault(); // 
 
-        // Obter valores dos campos
+        
         var nome = $('#nome').val();
         var email = $('#email').val();
         var telefone = $('#telefone').val();
         var cpf = $('#cpf').val();
         var endereco = $('#endereco').val();
-        var cep = $('#cep').val();
-
-        // Você pode fazer algo com os valores aqui, como enviar para um servidor
-        // Neste exemplo, apenas exibindo no console
+       
         console.log('Nome:', nome);
         console.log('E-mail:', email);
         console.log('Telefone:', telefone);
